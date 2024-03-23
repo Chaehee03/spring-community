e76bbfbc8cdb2e633c5e8048b924f68dc4ce1726
song_chaehee <sch003322@gmail.com>
2024-03-23 14:42:16 +0900

    chore: spring initializer init

---
+++ src/test/java/community/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package community.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
