a7aa01c711b9be9827bd8d0bc4c40621dda0e3a8
song_chaehee <sch003322@gmail.com>
2024-03-24 21:01:30 +0900

    feat: Photo entity 개발

---
+++ src/main/java/community/crud/domain/Photo.java
@@ -0,0 +1,23 @@
+package community.crud.domain;
+
+import community.crud.domain.Article;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+public class Photo {
+    @Id @GeneratedValue
+    @Column(name = "photo_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    private String filePath;
+    private String fileOriginName;
+    private String fileSaveName;
+}
