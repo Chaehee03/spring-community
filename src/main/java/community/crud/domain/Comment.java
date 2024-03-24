201d961b7d31e1a2b14fe7a32a7f9315b0d95d80
song_chaehee <sch003322@gmail.com>
2024-03-24 21:01:15 +0900

    feat: Comment entity 개발

---
+++ src/main/java/community/crud/domain/Comment.java
@@ -0,0 +1,30 @@
+package community.crud.domain;
+
+import community.crud.domain.Article;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+public class Comment {
+    @Id
+    @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private String paragraph;
+    private Long parentCommentId;
+
+
+}
