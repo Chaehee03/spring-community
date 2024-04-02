79aef67ad41ff9b1f4e94e53bdeae98b6d60b843
Chaehee03 <sch003322@gmail.com>
2024-04-02 23:14:20 +0900

    feat: DeleteArticleResponse DTO 개발

---
+++ src/main/java/community/crud/dto/article/DeleteArticleResponse.java
@@ -0,0 +1,12 @@
+package community.crud.dto.article;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteArticleResponse {
+    private boolean success;
+}
