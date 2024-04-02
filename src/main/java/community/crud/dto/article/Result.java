4dbe212a779000d23883897e118d842981666bcc
Chaehee03 <sch003322@gmail.com>
2024-04-02 23:15:21 +0900

    feat: Article 폴더 하의 Result DTO 개발

---
+++ src/main/java/community/crud/dto/article/Result.java
@@ -0,0 +1,13 @@
+package community.crud.dto.article;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result<T> {
+    private T data;
+    private int count;
+}
