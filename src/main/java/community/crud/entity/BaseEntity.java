cd1740bbe08323c966415743f4ca6c7dcfd80cbe
Chaehee03 <sch003322@gmail.com>
2024-04-02 23:09:36 +0900

    refactor: BaseEntity에 setter 추가

--- src/main/java/community/crud/entity/BaseEntity.java
+++ src/main/java/community/crud/entity/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@Getter
+@Getter @Setter
 public class BaseEntity {
     @CreationTimestamp
     @Column(updatable = false)
