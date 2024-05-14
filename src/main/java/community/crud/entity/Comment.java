8a472fb9bee02812e7480fbaf9b8fc43bb4a0c9d
Chaehee03 <sch003322@gmail.com>
2024-05-14 22:34:21 +0900

    feat: Comment entity 개발

--- src/main/java/community/crud/entity/Comment.java
+++ src/main/java/community/crud/entity/Comment.java
@@ -1,15 +1,20 @@
 package community.crud.entity;
 
+import community.crud.dto.article.ArticleRequest;
+import community.crud.dto.comment.CommentRequest;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDateTime;
 
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
-@Getter
-public class Comment {
+@Getter @Setter
+public class Comment extends BaseEntity{
     @Id
     @GeneratedValue
     @Column(name = "comment_id")
@@ -17,13 +22,25 @@ public class Comment {
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "article_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Article article;
 
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    @ManyToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
 
     private String paragraph;
     private Long parentCommentId;
 
+    // DTO -> entity
+    public static Comment toSaveEntity(CommentRequest commentDTO){
+        Comment comment = new Comment();
+        comment.setParagraph(commentDTO.getParagraph());
+        User user = new User();
+        user.setUserName(commentDTO.getWriter());
+        comment.setUser(user);
+        return comment;
+    }
 
 }
