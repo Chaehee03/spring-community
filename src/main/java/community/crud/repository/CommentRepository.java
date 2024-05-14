321f877ab5765c3ae4547581e533f1b3d642a5db
Chaehee03 <sch003322@gmail.com>
2024-05-14 22:35:28 +0900

    feat: CommentRepository 개발

---
+++ src/main/java/community/crud/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package community.crud.repository;
+
+import community.crud.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByArticleId(Long articleId);
+}
